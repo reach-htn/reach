5d4f4d3f048bf88f400754068f648979714b928b
Ryan Dancy <ryan@keal.ca>
2019-09-14 12:12:31 -0400

    Detect body of text

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+MENU_MSG = `Valid commands:
+* menu: show this menu`;
+// * wiki <title> ["/" page number]: read Wikipedia pages`
 
+exports.sms = functions.https.onRequest(async (req, res) => {
+  let msg = '';
+  let command = req.query.Body.toLowerCase();
+  
+  switch (command) {
+    case "menu":
+      msg = MENU_MSG;
+      break;
+    default:
+      msg = `Unrecognized command.\n${MENU_MSG}`;
+      break;
+  }
+  
+  res.send(`
+  <?xml version="1.0" encoding="UTF-8"?>
+  <Response>
+    <Sms>${msg}</Sms>
+  </Response>
+  `);
+});
+
+// basic test function
 exports.helloWorld = functions.https.onRequest(async (request, response) => {
  response.send(`<?xml version="1.0" encoding="UTF-8"?>
  <Response>
