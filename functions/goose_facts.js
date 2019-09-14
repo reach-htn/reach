dd99f5b2538d9e316b91fff9b7297a9354678c94
Michael Zhu <38388457+mikthepik@users.noreply.github.com>
2019-09-14 14:41:27 -0400

    Added functions and goose_facts.js
    added rng function(dice roll) and goose_facts function
    added goose_facts.js for it

---
+++ functions/goose_facts.js
@@ -0,0 +1,32 @@
+module.exports = {'facts': [
+  'There are around 30 species of goose in the world.',
+
+  'Geese are found all over the world.',
+
+  'In the wild, geese live in low marshes, river valleys, estuaries and wet pastures as well as on offshore islands, depending on the season.',
+
+  'Domesticated geese raised on farms forage on grassy pasture and are given yards, sometimes with sheds, as shelter.',
+
+  'They have a lifespan between 10 and 25 years in the wild. The average lifespan for domestic geese is around 20 to 25 years; but a number of geese individuals have been known to live for much longer.',
+
+  'Geese range in size from 60 to 120 centimeters (23.6 to 50 inches) in length and has a 83–185 cm (33–73 inches) wingspan. They weight from 1.5 to 12 kg (3.3 to 26.4 pounds).',
+
+  'A goose is often characterised by long neck, webbed feet, non-iridescent coloration and its honk.',
+
+  'Geese are tremendously social animals and the relationships that form in a flock are both amazing and amusing to watch.',
+
+  'Most of the world’s 30 or so species of geese are no strangers to migration, and some routinely accomplish amazing feats.',
+
+  'Migratory geese are capable of flying up to 5,000 kilometers (3,000 miles) each season.',
+
+  'Migratory geese fly in a V-shaped formation to minimize wind resistance and conserve energy. This formation also allows geese to remain in close proximity to one another during long journeys.',
+
+  'Geese fly at an average speed of about 65 kilometers (40 miles) per hour when migrating, but may increase their speed to 110 kilometers (70 miles) per hour if they catch a strong tailwind.',
+
+  'In Asia, Bar-headed Geese (Anser indicus) regularly migrate over the Himalayan Mountains, even over Mt. Everest at an altitude of 9,375 meters (30,750 feet) where the air is thin and the temperatures drop to -50°C (-60°F).',
+
+  'Geese honk while in flight to enable communication and encourage the flock to maintain a “V” formation.',
+
+  'Once a year, adult geese will lose flight and tail feathers, which is called molting. They are unable to fly until the new feathers have come in. The molt takes 30 to 45 days. Geese like to be near water during the molting season, to be able to escape from predators.',
+  
+]};
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const wiki = require('./wiki');
+const goose_facts = require('./goose_facts');
 
 MENU_MSG = `Valid commands:
 * menu: show this menu
@@ -10,12 +11,21 @@ exports.sms = functions.https.onRequest(async (req, res) => {
   let command = req.query.Body.toLowerCase();
   let commandName = command.split(/\s+/)[0];
   
+  let num = 0;
+  
   switch (command) {
     case 'menu':
       msg = MENU_MSG;
       break;
     case 'wiki':
       msg = wiki.execute();
+      break;
+    case 'roll': // rolls a dice with x amount of sides
+      num = command.split(/\s+/)[1];
+      msg = Math.floor(Math.random() * Math.floor(num)) + 1;
+      break;
+    case 'goosefacts': //spits out a ramdom goose fact
+      msg = goose_facts.facts(Math.floor(Math.random() * Math.floor(15)));
       break;
     default:
       msg = `Unrecognized command.\n${MENU_MSG}`;
