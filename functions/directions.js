febf35e6a190996ed1ced05d478dbb152c474d64
Kevin Li <34748712+Kleanur@users.noreply.github.com>
2019-09-14 14:31:29 -0400

    Update directions.js
    jank_irl

--- functions/directions.js
+++ functions/directions.js
@@ -1,6 +1,7 @@
 function getDirections(origin, dest){
-	origin.replaces(" ", "+");
-	dest.replaces(" ", "+");
+	origin.replace(" ", "+");
+	dest.replace(" ", "+");
+	
 	var geocode, text, url;
 	url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + dest + "&key=AIzaSyA0TF0woj5xZedSgnQGKy348BquHvvxraQ";
 	$.getJSON(url, function(data) {
@@ -11,6 +12,10 @@ function getDirections(origin, dest){
 			"\nTotal Trip Duration: " + geocode.routes[0].legs[0].duration.text;
 			
 	geocode.routes[0].legs[0].steps.forEach(function(item, index){
+		var instruction = item.html_instructions.replace("\u003cb\u003e", "");
+		instruction = item.html_instructions.replace("\u003c/b\u003e", "");
+		instruction = item.html_instructions.replace("\u003cdiv style=\"font-size:0.9em\"\u003e", "\n");
+		instruction = item.html_instructions.replace("\u003c/div\u003e", "");
 		text += "\n" + (index+1).toString() + ". " + item.html_instructions;
 	});
 	
