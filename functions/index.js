6458cf8f8915b057c4b7c50979e054735abc4338
Ryan Dancy <ryan@keal.ca>
2019-09-14 10:16:40 -0400

    Minimal working example

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest(async (request, response) => {
+ response.send(`<?xml version="1.0" encoding="UTF-8"?>
+ <Response>
+     <Sms>Hello from Reach!</Sms>
+ </Response>`);
+});
